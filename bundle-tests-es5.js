754713d3bdb70da2525ef84da59639d3fbaf5fcf
Romain Maneschi <romain@maneschi.fr>
2016-08-16 23:31:56 +0200

    Updates

---
+++ bundle-tests-es5.js
@@ -0,0 +1,13 @@
+"use strict";
+
+describe("A suite2", function () {
+    it("contains spec with an expectation", function () {
+        expect(true).toBe(true);
+    });
+});
+
+describe("A suite", function () {
+    it("contains spec with an expectation", function () {
+        expect(true).toBe(true);
+    });
+});
---
+++ bundle.js
@@ -0,0 +1,15 @@
+(function () {
+'use strict';
+
+class EventLoop {
+
+    static start() {
+        console.log('EventLoop created');
+    }
+
+}
+
+console.log('ok watch 22');
+EventLoop.start();
+
+}());
