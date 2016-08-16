95b20316513ac74f08ce9d614e05721786202961
Jérémy Flusin <flusin.jeremy@gmail.com>
2016-08-16 22:31:58 +0200

    changing gravity philosophy + test html for physics

--- physics.js
+++ physics.js
@@ -1,4 +1,6 @@
-const DEFAULT_GRAVITY = 5;
+const DEFAULT_GRAVITY = 1;
+const DEFAULT_VEL_X = 5;
+const DEFAULT_JUMP_VEL = -10;
 
 class Entity {
 
@@ -7,6 +9,8 @@ class Entity {
     this.y = y;
     this.width = width;
     this.height = height;
+    this.velX = 0;
+    this.velY = 0;
   }
 }
 
@@ -19,8 +23,25 @@ class Physics {
   	|| (eA.y + eA.height <= eB.y));
   }
 
-  static applyGravity(e, gravity){
-    gravity = gravity || DEFAULT_GRAVITY;
-    e.y += gravity;
+  static moveLeft(e, vel){
+    vel = vel || DEFAULT_VEL_X;
+    e.velX = -vel;
+  }
+
+  static moveRight(e, vel){
+    vel = vel || DEFAULT_VEL_X;
+    e.velX = vel;
+  }
+
+  static jump(e, jumpVel){
+    jumpVel = jumpVel || DEFAULT_JUMP_VEL;
+    e.velY += jumpVel;
+  }
+
+  static update(e){
+    e.x += e.velX;
+    e.velX = 0;
+    e.y += e.velY;
+    e.velY += DEFAULT_GRAVITY;
   }
 }
--- physicsSpec.js
+++ physicsSpec.js
@@ -9,12 +9,3 @@ let objC = new Entity(200, 200, 20, 20);
 console.log('Should be true:', Physics.checkCollision(objA, objB));
 console.log('Should be false:', Physics.checkCollision(objA, objC));
 console.log('Should be false:', Physics.checkCollision(objB, objC));
-
-//Let's apply gravity
-Physics.applyGravity(objA);
-Physics.applyGravity(objB, 8);
-Physics.applyGravity(objC, -2);
-
-console.log('Should be 5:', objA.y);
-console.log('Should be 18:', objB.y);
-console.log('Should be 198:', objC.y);
