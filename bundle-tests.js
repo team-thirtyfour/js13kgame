de750a314d51cd57979c6639e24d1c33fc90bb73
Romain Maneschi <romain@maneschi.fr>
2016-08-19 00:02:47 +0200

    Updates

--- bundle-tests.js
+++ bundle-tests.js
@@ -1,9 +1,3 @@
-describe("A suite2", function() {
-    it("contains spec with an expectation", function() {
-        expect(true).toBe(true);
-    });
-});
-
 describe("A suite", function() {
     it("contains spec with an expectation", function() {
         expect(true).toBe(true);
--- bundle.js
+++ bundle.js
@@ -1,181 +1,188 @@
 (function () {
 'use strict';
 
-const KeyboardCsts = {
-  UP_KEYCODE: 38,
-  LEFT_KEYCODE: 37,
-  RIGHT_KEYCODE: 39
-}
+class Level {
 
-let upPressed = false;
-let leftPressed = false;
-let rightPressed = false;
+    constructor (entities, gravity) {
+        this.entities = entities;
+        this.gravity = gravity;
 
-class Keyboard {
-
-  static _keyDownHandler(e){
-    switch(e.keyCode){
-      case KeyboardCsts.UP_KEYCODE:
-        upPressed = true;
-        break;
-      case KeyboardCsts.LEFT_KEYCODE:
-        leftPressed = true;
-        break;
-      case KeyboardCsts.RIGHT_KEYCODE:
-        rightPressed = true;
-        break;
-      default:
-        break;
+        this.movableEntities = this.entities.filter((e) => e.isMovable);
+        this.controllableEntities = this.entities.filter((e) => e.isControllable);
     }
-  }
 
-  static _keyUpHandler(e){
-    switch(e.keyCode){
-      case KeyboardCsts.UP_KEYCODE:
-        upPressed = false;
-        break;
-      case KeyboardCsts.LEFT_KEYCODE:
-        leftPressed = false;
-        break;
-      case KeyboardCsts.RIGHT_KEYCODE:
-        rightPressed = false;
-        break;
-      default:
-        break;
-    }
-  }
-
-  static isKeyPressed(keyCode){
-    switch(keyCode){
-      case KeyboardCsts.UP_KEYCODE:
-        return upPressed;
-        break;
-      case KeyboardCsts.LEFT_KEYCODE:
-        return leftPressed;
-        break;
-      case KeyboardCsts.RIGHT_KEYCODE:
-        return rightPressed;
-        break;
-      default:
-        break;
-    }
-  }
-
-}
-
-window.addEventListener('keydown', Keyboard._keyDownHandler, false);
-window.addEventListener('keyup', Keyboard._keyUpHandler, false);
-
-const DEFAULT_GRAVITY = 1;
-const DEFAULT_VEL_X = 5;
-const DEFAULT_JUMP_VEL = -10;
-
-class Physics {
-
-  static checkCollision(eA, eB){
-    return !((eA.x >= eB.x + eB.width)
-  	|| (eA.x + eA.width <= eB.x)
-  	|| (eA.y >= eB.y + eB.height)
-  	|| (eA.y + eA.height <= eB.y));
-  }
-
-  static moveLeft(e, vel){
-    vel = vel || DEFAULT_VEL_X;
-    e.velX = -vel;
-  }
-
-  static moveRight(e, vel){
-    vel = vel || DEFAULT_VEL_X;
-    e.velX = vel;
-  }
-
-  static jump(e, jumpVel){
-    jumpVel = jumpVel || DEFAULT_JUMP_VEL;
-    e.velY += jumpVel;
-  }
-
-  static update(e){
-    e.x += e.velX;
-    e.velX = 0;
-    e.y += e.velY;
-    e.velY += DEFAULT_GRAVITY;
-  }
 }
 
 class Entity {
 
-  constructor (x, y, width, height){
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-    this.velX = 0;
-    this.velY = 0;
-  }
-}
-
-//Will be the final 'FPS'
-const REFRESH_RATE = 60;
-
-var canvas = document.createElement('canvas');
-canvas.id = 'canvas';
-canvas.width = '600';
-canvas.height = '800';
-document.getElementsByTagName('body')[0].appendChild(canvas);
-
-
-var ctx = canvas.getContext('2d');
-var player = new Entity(100, 10, 10, 10);
-var ground = new Entity(0, 185, 50, 10);
-player.onGround = false;
-
-class EventLoop {
-
-    static start() {
-        EventLoop.loop();
-    }
-
-    static loop() {
-
-      if(Keyboard.isKeyPressed(KeyboardCsts.LEFT_KEYCODE)){
-        Physics.moveLeft(player);
-      }else if(Keyboard.isKeyPressed(KeyboardCsts.RIGHT_KEYCODE)){
-        Physics.moveRight(player);
-      }
-
-      if(Keyboard.isKeyPressed(KeyboardCsts.UP_KEYCODE) && player.onGround){
-        Physics.jump(player);
-      }
-
-      if(Physics.checkCollision(player, ground)){
-        player.velY = 0;
-        player.y = ground.y - player.height;
-      }
-
-      player.onGround =
-        (player.y === (ground.y - 10) && (player.x < ground.x + ground.width));
-
-
-      Physics.update(player);
-      EventLoop.render(player);
-      EventLoop.render(ground);
-
-      setTimeout(EventLoop.loop, (1 / REFRESH_RATE) * 1000);
-    }
-
-    static render(entity)
-    {
-        ctx.clearRect(0, 0, 600, 800);
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.rect(entity.x, entity.y, entity.width, entity.height);
-        ctx.closePath();
-        ctx.stroke();
+    constructor (x, y, width, height, velX, velY, collisionFactorX, collisionFactorY, forme, color, isMovable, isControllable) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.velX = velX;
+        this.velY = velY;
+        this.collisionFactorX = collisionFactorX;
+        this.collisionFactorY = collisionFactorY;
+        this.forme = forme;
+        this.color = color;
+        this.isMovable = isMovable;
+        this.isControllable = isControllable;
     }
 
 }
 
-console.log('ok watch 22');
-EventLoop.start();
+const levels = ["_:2.10...", "/:2.4..."]; // should be serialized levels array (TODO)
+
+var LevelDeserializer = (levelIndex) => {
+    return parse(levels[levelIndex]); // should be a Level
+}
+
+const parse = (levelStr) => {
+    //TODO
+    const player = new Entity(100, 10, 10, 10, 0, 0, 0, 0, 'circle', 'red', true, true);
+    const ground = new Entity(0, 185, 500, 10, 0, 0, 0, 2, 'square', 'blue', false, false);
+    return new Level([player, ground], 1);
+};
+
+//LEFT=37, UP=38, RIGHT=39
+let keys = [[], [], []];
+
+const keyHandler = () => {
+    return (e) => {
+        let i = e.keyCode - 37;
+        //key[37] can have a false value, so testing against undefined is needed
+        if (keys[i] !== undefined) {
+            keys[i].forEach((cb) => cb());
+        }
+    };
+};
+
+var Keyboard = {
+    onLeft: (cb) => keys[0].push(cb),
+    onUp: (cb) => keys[1].push(cb),
+    onRight: (cb) => keys[2].push(cb)
+}
+
+window.addEventListener('keyup', keyHandler(), false);
+window.addEventListener('keydown', keyHandler(), false);
+
+const X = 20;
+const JUMP = -10;
+
+var KeyboardController = (level) => {
+
+    Keyboard.onLeft(() => {
+        console.log('left');
+        level.controllableEntities.forEach((e) => e.velX = -X)
+    });
+    Keyboard.onRight(() => level.controllableEntities.forEach((e) => e.velX = X));
+    Keyboard.onUp(() => level.controllableEntities.forEach((e) => e.velY += JUMP));
+
+}
+
+const GRAVITY = 1;
+
+var Physics = {
+    update: (level) => {
+        level.movableEntities.forEach((e) => {
+            e.x += e.velX;
+            e.velX = 0;
+            e.y += e.velY;
+            e.velY += level.gravity || GRAVITY;
+        });
+    }
+}
+
+const render = (ctx, entity) => {
+    //TODO simple implementation, change me with entity form and color
+    ctx.clearRect(0, 0, 600, 800);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.rect(entity.x, entity.y, entity.width, entity.height);
+    ctx.fillStyle = entity.color;
+    ctx.fill();
+    ctx.closePath();
+    ctx.stroke();
+};
+
+var Renderer = {
+    render: (level, canvasMovable, canvasStatic) => {
+        const ctxMovable = canvasMovable.getContext('2d');
+        const ctxStatic = canvasStatic.getContext('2d');
+        level.entities.forEach((e) => {
+            if(e.isMovable) {
+                render(ctxMovable, e);
+            } else {
+                render(ctxStatic, e);//TODO init at start and not re-render each loop
+            }
+        });
+    }
+}
+
+const checkCollision = (eA, eB) => {
+    return !((eA.x >= eB.x + eB.width)
+    || (eA.x + eA.width <= eB.x)
+    || (eA.y >= eB.y + eB.height)
+    || (eA.y + eA.height <= eB.y));
+};
+
+var Collision = {
+
+    check: (level) => {
+        //TODO naive implementation == change me
+        let collision = false;
+        level.entities.forEach((eA) => {
+            level.entities.forEach((eB) => {
+                if(eA !== eB && checkCollision(eA, eB)) {
+                    //TODO not working
+                    eA.velX = eA.velX + (eA.velX * eB.collisionFactorX);
+                    eA.velY = eA.velY + (eA.velY * eB.collisionFactorY);
+                    eB.velX = eB.velX + (eB.velX * eA.collisionFactorX);
+                    eB.velY = eB.velY + (eB.velY * eA.collisionFactorY);
+                }
+            });
+        });
+        return collision;
+    }
+
+}
+
+const FRAMERATE = 60;
+
+var MainLoop = (level, canvasMovable, canvasStatic) => {
+
+  const loop = () => {
+
+    // On est pas sur de cet ordre
+    Physics.update(level);
+    Renderer.render(level, canvasMovable, canvasStatic);
+    Collision.check(level);
+
+    //Check la fin du niveau (mort ou a atteint la porte)
+    //c'est une collision dans tous les cas, a voir si on la fait
+    //directement dans Collision
+
+    setTimeout(loop, 1 / FRAMERATE * 1000);
+  };
+
+  loop();  
+}
+
+var canvasStatic = document.createElement('canvas');
+canvasStatic.width = '600';
+canvasStatic.height = '800';
+document.getElementsByTagName('body')[0].appendChild(canvasStatic);
+
+var canvasMovable = document.createElement('canvas');
+canvasMovable.width = '600';
+canvasMovable.height = '800';
+document.getElementsByTagName('body')[0].appendChild(canvasMovable);
+
+const levelIndex = 0;
+
+const level = LevelDeserializer(levelIndex);
+KeyboardController(level);
+MainLoop(level, canvasMovable, canvasStatic);
 
 }());
