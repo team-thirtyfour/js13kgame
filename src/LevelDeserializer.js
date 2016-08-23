7d2b007505a1dafad9059d5f85a6d34763046fd4
Romain Maneschi <romain@maneschi.fr>
2016-08-23 22:54:20 +0200

    close #38 : if map query is present deserialize map from it

--- src/LevelDeserializer.js
+++ src/LevelDeserializer.js
@@ -4,12 +4,30 @@ import {FORMS} from './Renderer';
 
 let levelIndex = 0;
 
+const getParameterByName = (name, url) => {
+    if (!url) {
+        url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) {
+        return null;
+    }
+    if (!results[2]) {
+        return '';
+    }
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+};
+
 export default {
     /**
      * return current level
      */
     level: () => {
-        return parse(levels[levelIndex]);
+        const map = getParameterByName('map');
+        const gravity = getParameterByName('gravity');
+        return parse(map && gravity ? [parseFloat(gravity), map] : levels[levelIndex]);
     },
     next: () => {
         if(levelIndex < levels.length - 1) {
@@ -27,14 +45,14 @@ export default {
 
 const parse = (level) => {
 
-    var entitiesStr = level[1].split('/');
+    var entitiesStr = level[1].split('|');
     var entities = entitiesStr.map((e)=>{
         var str = e.split(',');
         switch(str[0]){
             case 'J':
                 return Entity(+str[1], +str[2], 2, 2, 0, 0, 0, 0, FORMS.CIRCLE, 'red', true, true, false, false);
             case 'M':
-                return Entity(+str[1], +str[2], +str[3], +str[4], 0, 0, 0, 0, FORMS.RECT, 'grey', false, false, true, false);
+                return Entity(+str[1], +str[2], +str[3], +str[4], 0, 0, 0, 0, FORMS.RECT, 'grey', false, false, false, false);
             case 'T':
                 return Entity(+str[1], +str[2], +str[3], +str[4], 0, 0, 0, -0.5, FORMS.RECT, 'blue', false, false, false, false);
             case 'G':
@@ -52,11 +70,11 @@ const parse = (level) => {
  * first number : gravity
  * J,x,y : Joueur => new Entity(x, y, 2, 2, 0, 0, 0, 0, 'circle', 'red', true, true)
  * M,x,y,w,h : Mur => new Entity(x, y, w, h, 0, 0, 0, 0, 'square', 'grey', false, false)
- * T,x,y,w,h : Trampoline => new Entity(x, y, w, h, 0, 0, 0, -0.2, 'square', 'bleu', false, false)
+ * T,x,y,w,h : Trampoline => new Entity(x, y, w, h, 0, 0, 0, -0.2, 'square', 'blue', false, false)
  * G,x,y : Gate => new Entity(x, y, 3, 5, 0, 0, 0, -0.2, 'square', 'violet', false, false, false, true)
  */
 const levels = [
-    [1,'J,10,10/T,0,90,90,2/F,50,50,10,10'],
-    [1,'J,10,10/T,0,50,80,2/M,50,10,2,40/G,70,45/F,10,0,20,5/F,30,0,10,8/F,40,0,5,3/F,45,0,10,8/F,55,0,10,5'],
-    [1,'J,50,10/T,0,90,95,2/G,90,5/F,55,0,10,10']
+    [1,'J,10,10|T,0,90,90,2|F,50,50,10,10|G,90,90'],
+    [1,'J,10,10|T,0,50,80,2|M,50,10,2,40|G,70,45|F,10,0,20,5|F,30,0,10,8|F,40,0,5,3|F,45,0,10,3|F,55,0,10,5'],
+    [1,'J,50,10|T,0,90,95,2|G,90,5|F,55,0,10,10']
 ];
