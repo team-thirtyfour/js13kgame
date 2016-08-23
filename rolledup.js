d65a4f34425c826f120213767543d818f5c64410
Romain Maneschi <romain@maneschi.fr>
2016-08-23 23:42:25 +0200

    Size: 2081

--- rolledup.js
+++ rolledup.js
@@ -10,7 +10,7 @@ var Level = (entities, gravity) => {
     };
 };
 
-var Entity = (x, y, width, height, velX, velY, collisionFactorX, collisionFactorY, forme, color, isMovable, isControllable, isKiller, isFinisher) => {
+var Entity = (x, y, width, height, velX, velY, collisionFactorX, collisionFactorY, forme, color, isMovable, isControllable, isKiller, isFinisher, childFactory) => {
     return {
         x: x,
         y: y,
@@ -26,6 +26,7 @@ var Entity = (x, y, width, height, velX, velY, collisionFactorX, collisionFactor
         isControllable: isControllable,
         isKiller: isKiller,
         isFinisher: isFinisher,
+        childFactory: childFactory,
         isKilled: false,
         canJump: false
     };
@@ -158,13 +159,17 @@ const parse = (level) => {
             case 'J':
                 return Entity(+str[1], +str[2], 2, 2, 0, 0, 0, 0, FORMS.CIRCLE, 'red', true, true, false, false);
             case 'M':
-                return Entity(+str[1], +str[2], +str[3], +str[4], 0, 0, 0, 0, FORMS.RECT, 'grey', false, false, false, false);
+                return Entity(+str[1], +str[2], +str[3], +str[4], 0, 0, 0, -0.1, FORMS.RECT, 'grey', false, false, false, false);
             case 'T':
                 return Entity(+str[1], +str[2], +str[3], +str[4], 0, 0, 0, -0.5, FORMS.RECT, 'blue', false, false, false, false);
             case 'G':
                 return Entity(+str[1], +str[2], 3, 5, 0, 0, 0, 0, FORMS.RECT, 'violet', false, false, false, true);
             case 'F':
                 return Entity(+str[1], +str[2], +str[3], +str[4], 0, 0, 0, 0, FORMS.TRIANGLE_DOWN, 'orange', false, false, true, false);
+            case 'A':
+                return Entity(+str[1], +str[2], 2, 2, 0, 0, 0, 0, FORMS.CIRCLE, 'pink', false, false, true, false, () => {
+                    return Entity(50, 50, 0.5, 0.5, 0, 0, 0, 0, FORMS.CIRCLE, 'pink', true, false, true, false);
+                });
         }
 
     });
@@ -177,10 +182,12 @@ const parse = (level) => {
  * J,x,y : Joueur => new Entity(x, y, 2, 2, 0, 0, 0, 0, 'circle', 'red', true, true)
  * M,x,y,w,h : Mur => new Entity(x, y, w, h, 0, 0, 0, 0, 'square', 'grey', false, false)
  * T,x,y,w,h : Trampoline => new Entity(x, y, w, h, 0, 0, 0, -0.2, 'square', 'blue', false, false)
- * G,x,y : Gate => new Entity(x, y, 3, 5, 0, 0, 0, -0.2, 'square', 'violet', false, false, false, true)
+ * G,x,y : Gate => new Entity(x, y, 3, 5, 0, 0, 0, 0, 'square', 'violet', false, false, false, true)
+ * F,x,y,w,h : Fire
+ * A,x,y : Amo => new Entity(x, y, 3, 5, 0, 0, 0, 0, 'circle', 'pink', false, false, true, false)
  */
 const levels = [
-    [1,'J,10,10|T,0,90,90,2|F,50,50,10,10|G,90,90'],
+    [1,'J,10,10|T,0,90,90,2|G,90,90|A,50,50'],
     [1,'J,10,10|T,0,50,80,2|M,50,10,2,40|G,70,45|F,10,0,20,5|F,30,0,10,8|F,40,0,5,3|F,45,0,10,3|F,55,0,10,5'],
     [1,'J,50,10|T,0,90,95,2|G,90,5|F,55,0,10,10']
 ];
@@ -281,10 +288,13 @@ var Keyboard = (level) => {
 const FRAMERATE = 60;
 const rAF = requestAnimationFrame || function(cb) { setTimeout(cb, 1 / FRAMERATE * 1000); };
 
+let nbLoop = 0;
+
 var MainLoop = (level, onGameFinished, onGameOver) => {
 
     let lastTime = 0;
     const loop = () => {
+        nbLoop++;
 
         const now = Date.now();
         const deltaTime = (now - lastTime) / 1000.0;
@@ -302,6 +312,16 @@ var MainLoop = (level, onGameFinished, onGameOver) => {
         if(gameIsWin) {
             return onGameFinished();
         }
+        if(nbLoop % 25 === 0) {
+            level.entities
+                .filter((e) => e.childFactory !== undefined)
+                .forEach((e) => {
+                    const childEntity = e.childFactory();
+                    level.entities.push(childEntity);
+                    level.movableEntities.push(childEntity);
+                });
+        }
+
         Renderer.render(level);
 
         //Check la fin du niveau (mort ou a atteint la porte)
