1240886a6c88ecad121b5263d51c88d2a081cb84
Romain Maneschi <romain@maneschi.fr>
2016-08-23 23:40:04 +0200

    close #42 : first gun fire amo implementation

--- src/LevelDeserializer.js
+++ src/LevelDeserializer.js
@@ -59,6 +59,10 @@ const parse = (level) => {
                 return Entity(+str[1], +str[2], 3, 5, 0, 0, 0, 0, FORMS.RECT, 'violet', false, false, false, true);
             case 'F':
                 return Entity(+str[1], +str[2], +str[3], +str[4], 0, 0, 0, 0, FORMS.TRIANGLE_DOWN, 'orange', false, false, true, false);
+            case 'A':
+                return Entity(+str[1], +str[2], 2, 2, 0, 0, 0, 0, FORMS.CIRCLE, 'pink', false, false, true, false, () => {
+                    return Entity(50, 50, 0.5, 0.5, 0, 0, 0, 0, FORMS.CIRCLE, 'pink', true, false, true, false);
+                });
         }
 
     });
@@ -71,10 +75,12 @@ const parse = (level) => {
  * J,x,y : Joueur => new Entity(x, y, 2, 2, 0, 0, 0, 0, 'circle', 'red', true, true)
  * M,x,y,w,h : Mur => new Entity(x, y, w, h, 0, 0, 0, 0, 'square', 'grey', false, false)
  * T,x,y,w,h : Trampoline => new Entity(x, y, w, h, 0, 0, 0, -0.2, 'square', 'blue', false, false)
- * G,x,y : Gate => new Entity(x, y, 3, 5, 0, 0, 0, -0.2, 'square', 'violet', false, false, false, true)
+ * G,x,y : Gate => new Entity(x, y, 3, 5, 0, 0, 0, 0, 'square', 'violet', false, false, false, true)
+ * F,x,y,w,h : Fire
+ * A,x,y : Amo => new Entity(x, y, 3, 5, 0, 0, 0, 0, 'circle', 'pink', false, false, true, false)
  */
 const levels = [
-    [1,'J,10,10|T,0,90,90,2|F,50,50,10,10|G,90,90'],
+    [1,'J,10,10|T,0,90,90,2|G,90,90|A,50,50'],
     [1,'J,10,10|T,0,50,80,2|M,50,10,2,40|G,70,45|F,10,0,20,5|F,30,0,10,8|F,40,0,5,3|F,45,0,10,3|F,55,0,10,5'],
     [1,'J,50,10|T,0,90,95,2|G,90,5|F,55,0,10,10']
 ];
--- src/MainLoop.js
+++ src/MainLoop.js
@@ -6,10 +6,13 @@ import Keyboard from './Keyboard';
 const FRAMERATE = 60;
 const rAF = requestAnimationFrame || function(cb) { setTimeout(cb, 1 / FRAMERATE * 1000); };
 
+let nbLoop = 0;
+
 export default (level, onGameFinished, onGameOver) => {
 
     let lastTime = 0;
     const loop = () => {
+        nbLoop++;
 
         const now = Date.now();
         const deltaTime = (now - lastTime) / 1000.0;
@@ -27,6 +30,16 @@ export default (level, onGameFinished, onGameOver) => {
         if(gameIsWin) {
             return onGameFinished();
         }
+        if(nbLoop % 25 === 0) {
+            level.entities
+                .filter((e) => e.childFactory !== undefined)
+                .forEach((e) => {
+                    const childEntity = e.childFactory();
+                    level.entities.push(childEntity);
+                    level.movableEntities.push(childEntity);
+                });
+        }
+
         Renderer.render(level);
 
         //Check la fin du niveau (mort ou a atteint la porte)
