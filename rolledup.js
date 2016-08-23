6607f80a7b2cc73d85ed3cb5e9f154e55643d3b2
Romain Maneschi <romain@maneschi.fr>
2016-08-23 23:01:34 +0200

    Size: 1980

--- rolledup.js
+++ rolledup.js
@@ -8,7 +8,7 @@ var Level = (entities, gravity) => {
         movableEntities: entities.filter((e) => e.isMovable),
         controllableEntities: entities.filter((e) => e.isControllable)
     };
-}
+};
 
 var Entity = (x, y, width, height, velX, velY, collisionFactorX, collisionFactorY, forme, color, isMovable, isControllable, isKiller, isFinisher) => {
     return {
@@ -26,29 +26,37 @@ var Entity = (x, y, width, height, velX, velY, collisionFactorX, collisionFactor
         isControllable: isControllable,
         isKiller: isKiller,
         isFinisher: isFinisher,
-
         isKilled: false,
         canJump: false
     };
-}
+};
 
-const FORME_RECT = 0;
-const FORME_CIRCLE = 1;
-const FORME_TRIANGLE = 2;
+const FORMS = {
+    RECT: 0,
+    CIRCLE: 1,
+    TRIANGLE: 2,
+    TRIANGLE_DOWN: 3
+};
+
+let needToDrawStatic = true;
 
 const render = (canvas, ctx, entity) => {
     //TODO simple implementation, change me with entity form and color
     const relative = compute(canvas, entity);
     ctx.fillStyle = entity.color;
     ctx.beginPath();
-    if(entity.forme === FORME_RECT) {
+    if(entity.forme === FORMS.RECT) {
         ctx.rect(relative.x, relative.y, relative.width, relative.height);
-    } else if(entity.forme === FORME_CIRCLE) {
+    } else if(entity.forme === FORMS.CIRCLE) {
         ctx.arc(relative.x+relative.width/2, relative.y+relative.height/2, relative.width/2, 0, 2*Math.PI);
-    } else if(entity.forme === FORME_TRIANGLE) {
+    } else if(entity.forme === FORMS.TRIANGLE) {
         ctx.moveTo(relative.x+relative.width/2, relative.y);
         ctx.lineTo(relative.x+relative.width, relative.y+relative.height);
-        ctx.lineTo(relative.x, relative.y+relative.height)
+        ctx.lineTo(relative.x, relative.y+relative.height);
+    } else if(entity.forme === FORMS.TRIANGLE_DOWN) {
+        ctx.moveTo(relative.x, relative.y);
+        ctx.lineTo(relative.x+relative.width, relative.y);
+        ctx.lineTo(relative.x+relative.width/2, relative.y+relative.height);
     }
     ctx.closePath();
     ctx.fill();
@@ -60,7 +68,7 @@ const compute = (canvas, entity) => {
         y: entity.y * canvas.height / 100,
         width: entity.width * canvas.width / 100,
         height: entity.height * canvas.height / 100
-    }
+    };
 };
 
 const draw = (canvas, entities) => {
@@ -69,17 +77,18 @@ const draw = (canvas, entities) => {
     entities.forEach((e) => render(canvas, ctx, e));
 };
 
-var Renderer$1 = {
-
-    init: (level) => {
-        draw(canvasMovable, level.entities.filter((e) => e.isMovable));
-        draw(canvasStatic, level.entities.filter((e) => !e.isMovable));
+var Renderer = {
+    init: () => {
+        needToDrawStatic = true;
     },
-
     render: (level) => {
+        if(needToDrawStatic) {
+            needToDrawStatic = false;
+            draw(canvasStatic, level.entities.filter((e) => !e.isMovable));
+        }
         draw(canvasMovable, level.entities.filter((e) => e.isMovable));
     }
-}
+};
 
 var canvasStatic = document.createElement('canvas');
 canvasStatic.width = window.innerWidth * 0.8;
@@ -96,17 +105,35 @@ window.addEventListener('resize', () => {
     canvasStatic.height = window.innerHeight * 0.8;
     canvasMovable.width = window.innerWidth * 0.8;
     canvasMovable.height = window.innerHeight * 0.8;
-    Renderer.init(level, canvasMovable, canvasStatic);
+    needToDrawStatic = true;
 }, false);
 
 let levelIndex = 0;
+
+const getParameterByName = (name, url) => {
+    if (!url) {
+        url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) {
+        return null;
+    }
+    if (!results[2]) {
+        return '';
+    }
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+};
 
 var LevelDeserializer = {
     /**
      * return current level
      */
     level: () => {
-        return parse(levels[levelIndex]);
+        const map = getParameterByName('map');
+        const gravity = getParameterByName('gravity');
+        return parse(map && gravity ? [parseFloat(gravity), map] : levels[levelIndex]);
     },
     next: () => {
         if(levelIndex < levels.length - 1) {
@@ -120,22 +147,24 @@ var LevelDeserializer = {
             levelIndex--;
         }
     }
-}
+};
 
 const parse = (level) => {
 
-    var entitiesStr = level[1].split('/');
+    var entitiesStr = level[1].split('|');
     var entities = entitiesStr.map((e)=>{
         var str = e.split(',');
         switch(str[0]){
             case 'J':
-                return Entity(parseFloat(str[1]), parseFloat(str[2]), 2, 2, 0, 0, 0, 0, FORME_CIRCLE, 'red', true, true, false, false);
+                return Entity(+str[1], +str[2], 2, 2, 0, 0, 0, 0, FORMS.CIRCLE, 'red', true, true, false, false);
             case 'M':
-                return Entity(parseFloat(str[1]), parseFloat(str[2]), parseFloat(str[3]), parseFloat(str[4]), 0, 0, 0, 0, FORME_TRIANGLE, 'grey', false, false, true, false);
+                return Entity(+str[1], +str[2], +str[3], +str[4], 0, 0, 0, 0, FORMS.RECT, 'grey', false, false, false, false);
             case 'T':
-                return Entity(parseFloat(str[1]), parseFloat(str[2]), parseFloat(str[3]), parseFloat(str[4]), 0, 0, 0, -0.5, FORME_RECT, 'blue', false, false, false, false);
+                return Entity(+str[1], +str[2], +str[3], +str[4], 0, 0, 0, -0.5, FORMS.RECT, 'blue', false, false, false, false);
             case 'G':
-                return Entity(parseFloat(str[1]), parseFloat(str[2]), 3, 5, 0, 0, 0, 0, FORME_RECT, 'violet', false, false, false, true);
+                return Entity(+str[1], +str[2], 3, 5, 0, 0, 0, 0, FORMS.RECT, 'violet', false, false, false, true);
+            case 'F':
+                return Entity(+str[1], +str[2], +str[3], +str[4], 0, 0, 0, 0, FORMS.TRIANGLE_DOWN, 'orange', false, false, true, false);
         }
 
     });
@@ -147,35 +176,32 @@ const parse = (level) => {
  * first number : gravity
  * J,x,y : Joueur => new Entity(x, y, 2, 2, 0, 0, 0, 0, 'circle', 'red', true, true)
  * M,x,y,w,h : Mur => new Entity(x, y, w, h, 0, 0, 0, 0, 'square', 'grey', false, false)
- * T,x,y,w,h : Trampoline => new Entity(x, y, w, h, 0, 0, 0, -0.2, 'square', 'bleu', false, false)
+ * T,x,y,w,h : Trampoline => new Entity(x, y, w, h, 0, 0, 0, -0.2, 'square', 'blue', false, false)
+ * G,x,y : Gate => new Entity(x, y, 3, 5, 0, 0, 0, -0.2, 'square', 'violet', false, false, false, true)
  */
 const levels = [
-      //level 1
-    [0.05,'J,10,10/T,0,50,80,2/M,50,10,2,40/G,70,45' ]
-    , // level 2
-    [0.05,'J,50,10/T,0,90,95,2/G,90,5']
+    [1,'J,10,10|T,0,90,90,2|F,50,50,10,10|G,90,90'],
+    [1,'J,10,10|T,0,50,80,2|M,50,10,2,40|G,70,45|F,10,0,20,5|F,30,0,10,8|F,40,0,5,3|F,45,0,10,3|F,55,0,10,5'],
+    [1,'J,50,10|T,0,90,95,2|G,90,5|F,55,0,10,10']
 ];
 
-var Console = (level) => {
+var Console = (/*level*/) => {
 
-}
+};
 
 var Physics = {
-    update: (level) => {
+    update: (level, deltaTime) => {
         level.movableEntities.forEach((e) => {
-            e.x += e.velX;
+            e.x += deltaTime * e.velX;
             e.velX = 0;
-            e.y += e.velY;
+            e.y += deltaTime * e.velY;
             e.velY += level.gravity;
         });
     }
-}
+};
 
 const checkCollision = (eA, eB) => {
-    return !((eA.x >= eB.x + eB.width)
-    || (eA.x + eA.width <= eB.x)
-    || (eA.y >= eB.y + eB.height)
-    || (eA.y + eA.height <= eB.y));
+    return !((eA.x >= eB.x + eB.width) || (eA.x + eA.width <= eB.x) || (eA.y >= eB.y + eB.height) || (eA.y + eA.height <= eB.y));
 };
 
 var Collision = {
@@ -186,7 +212,7 @@ var Collision = {
      * @returns {boolean} true when game is won
      */
     check: (level) => {
-        let gameIsWin = false;
+        let gameIsWon = false;
         //TODO naive implementation == change me
         level.entities.forEach((eA) => {
             eA.canJump = false;
@@ -205,20 +231,18 @@ var Collision = {
                         eA.isKilled = true;
                     }
 
-                    if(eB.isFinisher) {
-                        gameIsWin = true;
-                    }
+                    gameIsWon = eB.isFinisher;
                 }
             });
         });
-        return gameIsWin;
+        return gameIsWon;
     },
     checkGameOver: (level) => {
         // when 1 controllable entity is outside of game
-        return !!level.controllableEntities.find((e) => e.x > 100 || e.x < 0 || e.y > 100 || e.y < 0);
+        return !!level.controllableEntities.find((e) => e.isKilled || e.x > 100 || e.x < 0 || e.y > 100 || e.y < 0);
     }
 
-}
+};
 
 //LEFT=37, UP=38, RIGHT=39
 let keys = [false, false, false];
@@ -236,8 +260,8 @@ const keyHandler = (val) => {
 window.addEventListener('keydown', keyHandler(true), false);
 window.addEventListener('keyup', keyHandler(false), false);
 
-const X = 1;
-const JUMP = -2;
+const X = 50;
+const JUMP = -50;
 
 var Keyboard = (level) => {
 
@@ -252,19 +276,15 @@ var Keyboard = (level) => {
             e.velY += JUMP;
         }
     });
-}
+};
 
 const FRAMERATE = 60;
-const rAF = requestAnimationFrame || function(cb) { setTimeout(cb, 1 / FRAMERATE * 1000) };
+const rAF = requestAnimationFrame || function(cb) { setTimeout(cb, 1 / FRAMERATE * 1000); };
 
 var MainLoop = (level, onGameFinished, onGameOver) => {
 
-    let lastTime;
+    let lastTime = 0;
     const loop = () => {
-
-        if(Collision.checkGameOver(level)) {
-            return onGameOver();
-        }
 
         const now = Date.now();
         const deltaTime = (now - lastTime) / 1000.0;
@@ -272,13 +292,17 @@ var MainLoop = (level, onGameFinished, onGameOver) => {
         Keyboard(level);
 
         // On est pas sur de cet ordre
-        // TODO use delta time in update
         Physics.update(level, deltaTime);
+
+        if(Collision.checkGameOver(level) && lastTime > 0) {
+            return onGameOver();
+        }
+
         const gameIsWin = Collision.check(level);
         if(gameIsWin) {
             return onGameFinished();
         }
-        Renderer$1.render(level);
+        Renderer.render(level);
 
         //Check la fin du niveau (mort ou a atteint la porte)
         //c'est une collision dans tous les cas, a voir si on la fait
@@ -288,10 +312,10 @@ var MainLoop = (level, onGameFinished, onGameOver) => {
         rAF(loop);
     };
 
-    Renderer$1.init(level);
+    Renderer.init();
     loop();
 
-}
+};
 
 const win = () => {
     LevelDeserializer.next();
